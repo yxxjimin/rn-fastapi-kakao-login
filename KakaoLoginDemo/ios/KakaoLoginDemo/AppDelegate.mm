a8ec0f72763a6e17c73f32e606010b1af8603f71
Jimin Yoo <jmy0819_@naver.com>
2023-12-03 23:38:01 +0900

    init: Configurations

--- KakaoLoginDemo/ios/KakaoLoginDemo/AppDelegate.mm
+++ KakaoLoginDemo/ios/KakaoLoginDemo/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+#import <RNKakaoLogins.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -21,6 +23,16 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+// Kakao
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+ return NO;
 }
 
 @end
---
+++ KakaoLoginDemo/ios/SwiftBridge.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftBridge.swift
+//  KakaoLoginDemo
+//
+//  Created by 유지민 on 12/3/23.
+//
+
+import Foundation
